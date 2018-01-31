3f3fb5d91b3c5e043a89b9734d3c08c6bec3a042
ixtlarle <SpaceCakes9@DESKTOP-QOKEHQE>
2018-01-31 16:56:20 -0600

    created first view which takes in a model to dynamically display items
    added a viewmodel folder and viewmodel class to facilitate in the passing of data from controller to view .

--- OliversPieShop/Controllers/PieController.cs
+++ OliversPieShop/Controllers/PieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OliversPieShop.Models;
+using OliversPieShop.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,7 +24,11 @@ namespace OliversPieShop.Controllers
         
         public ViewResult List()
         {
-            return View(_pieRepository.Pies);
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.Pies;
+            piesListViewModel.CurrentCategory = "bacon bits";
+
+            return View(piesListViewModel);
         }
     }
 }
