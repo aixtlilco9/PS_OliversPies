d93bc0f49885d6892fa5c7a1f9b39e0d26e166e0
ixtlarle <SpaceCakes9@DESKTOP-QOKEHQE>
2018-01-31 23:21:17 -0600

    created a appdbcontext file in models
    created a appsetting file in project root to house the connection sttring which was automatically generated i just added a db name
    
    also added some more packages using nuget package manager since the proj.json is no longer a thing...
    
    so i spend a couple hours trying to see why usesqlserver in startup.cs wasnt worknig turned out i didnt have microsoft.ef.sql package installed and later turned out the latest version wasnt compatible with core 1.1 which is wat im sopposedly using so i had to install a older version

--- OliversPieShop/Startup.cs
+++ OliversPieShop/Startup.cs
@@ -8,15 +8,31 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OliversPieShop.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace OliversPieShop
 {
     public class Startup
     {
+        //A.I added private iconfigroot _configroot with the using above
+        private IConfigurationRoot _configurationRoot;
+
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            _configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                                         options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
+
             //a.i: added addmvc 
             services.AddTransient<ICategoryRepository, MockCategoryRepository>();
             services.AddTransient<IPieRepository, MockPieRepository>();
